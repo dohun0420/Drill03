050d072ecbe6d972be1ed32607c6184a75b6a8a1
dohun0420 <154303870+dohun0420@users.noreply.github.com>
2024-09-24 10:03:01 +0900

    캐릭터 생성 정상

--- Lecture04_2D_Rendering/character_grass.py
+++ Lecture04_2D_Rendering/character_grass.py
@@ -1,21 +1,26 @@
 from pico2d import *
 
+open_canvas()
+
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
 def run_rectangle():
     print('RECTANGLE')
     pass
 
 def run_circle():
     print('CIRCLE')
+    
+    clear_canvas_now()
+    boy.draw_now(400, 300)
+    delay(1)
     pass
-
-open_canvas()
-
-grass = load.image('grass.png')
-boy = load.image('character.png')
 
 while True:
     run_rectangle()
     run_circle()
+    break
     
 
 close_canvas()
