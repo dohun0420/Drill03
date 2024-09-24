5cd3a2c012d848378b96e04f09e50ea43781819a
dohun0420 <154303870+dohun0420@users.noreply.github.com>
2024-09-24 10:42:40 +0900

    draw_boy 변수 선언 및 run_top 구현, run_circle 수정

--- Lecture04_2D_Rendering/character_grass.py
+++ Lecture04_2D_Rendering/character_grass.py
@@ -6,8 +6,15 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+def draw_boy(x, y):
+    clear_canvas_now()
+    boy.draw_now(x, y)
+    delay(0.01)
+    
 def run_top():
-    pass
+    for x in range(0, 800, 10):
+        draw_boy(x, 550)
+
 def run_right():
     pass
 def run_bottom():
@@ -30,14 +37,11 @@ def run_circle():
     for d in range(0, 360):
         x = r * math.cos(math.radians(d)) + cx
         y = r * math.sin(math.radians(d)) + cy
-        
-        clear_canvas_now()
-        boy.draw_now(x, y)
-        delay(0.01)
+        draw_boy(x, y)
 
 while True:
     run_rectangle()
-    # run_circle()
+    run_circle()
     break
     
 
